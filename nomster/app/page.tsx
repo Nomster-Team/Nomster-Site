1c96b010bd5be915d45b6e1531e866b440316afc
Chuyang Zhang <Chuyang Zhang>
2024-10-19 18:06:20 -0400

    Made the stuff

--- nomster/app/page.tsx
+++ nomster/app/page.tsx
@@ -1,16 +1,22 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
+import Image from 'next/image'
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 
 export default async function Index() {
   return (
     <>
-      <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
         <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
+        <div className="mt-8 space-x-4">
+        </div>
+        <a href="#" className="inline-block">
+            <Image
+              src="/google-play-badge.png"
+              alt="Get it on Google Play"
+              width={135}
+              height={40}
+            />
+          </a>
+      </main> 
     </>
   );
 }
