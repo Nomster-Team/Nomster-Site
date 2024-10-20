12164e49fde6ac59bb3d391b41aa88559e9267f2
Chuyang Zhang <chuyangzhanggeorge@gmail.com>
2024-10-20 19:48:28 -0400

    Changed email

--- nomster/app/api/send-email/route.ts
+++ nomster/app/api/send-email/route.ts
@@ -4,8 +4,7 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
-  const { email } = await request.json();
-  const { name } = await request.json();
+  const { email, name } = await request.json();
   const { data, error } = await resend.emails.send({
     from: 'adam@nomster.me',
     to: email,
