b182bda1776f41e7075718003af579dbd7cf57fc
Chuyang Zhang <chuyangzhanggeorge@gmail.com>
2024-10-20 00:23:27 -0400

    Changed the Public KEy

--- nomster/app/layout.tsx
+++ nomster/app/layout.tsx
@@ -8,12 +8,11 @@ import { useState } from 'react';
 import { createClient } from "@supabase/supabase-js"
 
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error("Missing Supabase environment variables");
-}
+console.log('Supabase URL:', supabaseUrl); // Debugging line
+console.log('Supabase Key:', supabaseKey); // Debugging line
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 
