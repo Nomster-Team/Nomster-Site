7ed01f654e62964142332d1debe9f12f9b2a3dd6
Chuyang Zhang <69881086+oddblaster@users.noreply.github.com>
2024-10-20 00:11:10 -0400

    Update layout.tsx

--- nomster/app/layout.tsx
+++ nomster/app/layout.tsx
@@ -9,7 +9,6 @@ import { createClient } from "@supabase/supabase-js"
 
 
 const supabaseUrl = process.env.SUPABASE_URL;
-console.log(supabaseUrl)
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
