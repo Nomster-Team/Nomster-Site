3c6940f818001cad6cf656f6b9147466658c43f8
Chuyang Zhang <Chuyang Zhang>
2024-10-19 23:40:31 -0400

    Fixed

--- nomster/app/layout.tsx
+++ nomster/app/layout.tsx
@@ -11,6 +11,10 @@ import { createClient } from "@supabase/supabase-js"
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 console.log(supabaseUrl)
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Missing Supabase environment variables");
+}
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 
