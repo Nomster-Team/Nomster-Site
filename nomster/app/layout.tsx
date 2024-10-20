c133ad1f7bead68d3488e332e2f30600acf2194b
Chuyang Zhang <69881086+oddblaster@users.noreply.github.com>
2024-10-20 00:45:35 -0400

    Update layout.tsx

--- nomster/app/layout.tsx
+++ nomster/app/layout.tsx
@@ -8,6 +8,7 @@ import { useState } from 'react';
 import { createClient } from "@supabase/supabase-js"
 
 
+
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 const supabase = createClient(supabaseUrl, supabaseKey);
